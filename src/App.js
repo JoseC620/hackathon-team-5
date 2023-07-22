e65d2b6c1adad5f54b92fb4d8f79cac337ab6ca1
JoseC620 <Jcepeda6201@gmail.com>
2023-07-22 16:20:38 -0400

    got bootstrap workin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Nav />
+      </Router>
     </div>
   );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,11 @@
 import { Navbar } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 
 export default function Nav() {
-    <Navbar>
-       
+  return(
+    <Navbar bg='primary' expand='lg' varient='light'>
+        <Link style={{color: "Black"}}>Hello</Link>
     </Navbar>
+  )
 }
