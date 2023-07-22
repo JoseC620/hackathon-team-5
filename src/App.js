06d670efbccdb8621e5171e580910fc0be0aff02
Wilsantos1975 <Faustosantos@pursuit.org>
2023-07-22 18:07:48 -0400

    creating routes for different views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Nav from './Components/Nav';
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/parks" element={<Parks />} />
+          <Route path="/parks/:parkId" element={<ShowPark />} />
+          <Route path="*" element={<FourZeroFour />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/Components/Parks.js
@@ -0,0 +1,18 @@
+
+import  React from "react";
+import axios from "axios";
+import {useState , useEffect} from "react";
+
+// import the API here from the utils folder, .env file, or wherever you are storing it
+
+function Parks() {
+    return (
+        <div>
+            <h1>Parks</h1>
+        </div>
+
+
+}
+
+export default Parks;
+
