e85aab39579a121e663af76ec55099899fe43762
shareekaepps <shareekaepps@icloud.com>
2023-07-23 02:42:02 -0400

    commiting multiple wroking CSS changes thus far. installed google fonts, react-icons, mui icons, and made app mobile firendly thus far.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './Components/Nav';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Nav />
+        <Home />
+      {/* <Nav /> */}
       </Router>
     </div>
   );
