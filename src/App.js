6d85a27fda07ed6e835d99408ae440e4451d33d5
shareekaepps <shareekaepps@icloud.com>
2023-07-23 03:54:12 -0400

    implemented About page, added more css styling, will explore functionality possibilites with team.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './Components/Nav';
 import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Home />
+        <About />
       {/* <Nav /> */}
       </Router>
     </div>
