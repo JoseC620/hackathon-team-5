d73ac40f92baf1db7af45c06daa8b2360edf6deb
Wilsantos1975 <Faustosantos@pursuit.org>
2023-07-23 10:29:21 -0400

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Parks from "./Pages/Parks";
+import ShowPark from "./Pages/ShowPark";
+import FourZeroFour from "./Pages/FourZeroFour";
+import Index from "./Pages/Index";
+
 
 function App() {
   return (
@@ -10,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/parks" element={<Parks />} />
+          <Route path="/parks" element={<Index />} />
           <Route path="/parks/:parkId" element={<ShowPark />} />
           <Route path="*" element={<FourZeroFour />} />
         </Routes>
