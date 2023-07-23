72695f740ed9c57ddb6b12004922e7a529d8d9c2
JoseC620 <Jcepeda6201@gmail.com>
2023-07-23 16:38:41 -0400

    added list of parks and some css

--- src/Components/Parks.js
+++ src/Components/Parks.js
@@ -1,15 +1,32 @@
 
 import  React from "react";
-import axios from "axios";
+import { Link } from "react-router-dom";
 import {useState , useEffect} from "react";
+import { getParkData } from "../api/fetch"
 import Park from "./Park";
 
 // import the API here from the utils folder, .env file, or wherever you are storing it
 
 function Parks() {
+
+    const [parks,setParks] = useState([])
+
+    useEffect(() => {
+        getParkData().then((response) => {
+          setParks(response)
+        })
+      }, [])
+
     return (
         <div>
-            <Park/>
+            <table class="table">
+                <thead>
+                    
+           {parks.map((park) => {
+            return <Park key={park.globalid} park={park}/>
+           })}
+                </thead>
+            </table>
         </div>
     )
 
