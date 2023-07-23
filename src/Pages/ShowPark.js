d6b50065bb5328ba88aac2f31c35abaceae31f27
aardvarkpepper <aardvarkpepper@gmail.com>
2023-07-23 11:53:38 -0400

    Add useParams 'parkId' to ShowPark.js

--- src/Pages/ShowPark.js
+++ src/Pages/ShowPark.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Park from "../Components/Park.js";
+import { useParams, useLocation } from "react-router-dom";
 
 function ShowPark() {
 
+    const { parkId } = useParams();
+
     return (
         <div>
-        <h1>ShowPark</h1>
+        <Park parkId={parkId} />
         </div>
     );
     }
